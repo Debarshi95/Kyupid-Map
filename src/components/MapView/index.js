085d853ca8550ecf01d9bd677e6cdfdbc845c0fa
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2021-12-29 16:22:49 +0530

    Added MapView component

---
+++ src/components/MapView/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+const MapView = ({ position, children }) => {
+  return (
+    <div className="">
+      <MapContainer
+        center={position}
+        zoom={10}
+        doubleClickZoom={false}
+        maxZoom={12}
+        minZoom={10}
+        className="h-96"
+      >
+        {children}
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapView;
